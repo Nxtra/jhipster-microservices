085aa5d2546029e7e4c9686e5d218d5a0b35f243
Nxtra <nickvanhoof1@gmail.com>
2019-05-16 14:25:43 +0200

    add k8s config

---
+++ k8s/kubectl-apply.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Files are ordered in proper order with needed wait for the dependent custom resource definitions to get initialized.
+# Usage: bash kubectl-apply.sh
+
+logSummary(){
+    echo ""
+    echo "#####################################################"
+    echo "Please find the below useful endpoints,"
+    echo "JHipster Grafana - http://jhipster-grafana.default"
+    echo "#####################################################"
+}
+
+kubectl label namespace default istio-injection=enabled --overwrite=true
+kubectl apply -f notification/
+kubectl apply -f store/
+kubectl apply -f monitoring/jhipster-prometheus-crd.yml
+until [ $(kubectl get crd prometheuses.monitoring.coreos.com 2>>/dev/null | wc -l) -ge 2 ]; do
+    echo "Waiting for the custom resource prometheus operator to get initialised";
+    sleep 5;
+done
+kubectl apply -f monitoring/jhipster-prometheus-cr.yml
+kubectl apply -f monitoring/jhipster-grafana.yml
+kubectl apply -f monitoring/jhipster-grafana-dashboard.yml
+
+logSummary
